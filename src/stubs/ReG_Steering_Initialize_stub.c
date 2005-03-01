1b62a63e9e41cd0395fd359763631c8d76306543
Andrew Porter <andrew.porter@manchester.ac.uk>
2005-03-01 16:26:25 +0000

    First work on stub files to avoid problems when compiling dynamic lib and have no f90 compiler available.

---
+++ src/stubs/ReG_Steering_Initialize_stub.c
@@ -0,0 +1,8 @@
+#include "ReG_Steer_types.h"
+
+void steering_initialize_f(char *AppName, int NumSupportedCmds,
+                           int *SupportedCmds, int *Status)
+{
+  *Status = REG_SUCCESS;
+  return;
+}
