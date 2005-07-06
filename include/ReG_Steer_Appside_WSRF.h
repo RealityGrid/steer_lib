74fd110378dfbd43f7af4eaaa70fce8c42f0396e
Andrew Porter <andrew.porter@manchester.ac.uk>
2005-07-06 14:28:09 +0000

    New WSRF-related headers

---
+++ include/ReG_Steer_Appside_WSRF.h
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------------------
+  This header file contains routines and data structures for
+  application-side WSRF, SOAP-based communication.
+
+  (C) Copyright 2005, University of Manchester, United Kingdom,
+  all rights reserved.
+
+  This software was developed by the RealityGrid project
+  (http://www.realitygrid.org), funded by the EPSRC under grants
+  GR/R67699/01 and GR/R67699/02.
+
+  LICENCE TERMS
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+  THIS MATERIAL IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. THE ENTIRE RISK AS TO THE QUALITY
+  AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE PROGRAM PROVE
+  DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR
+  CORRECTION.
+
+  Authors........: Andrew Porter
+
+---------------------------------------------------------------------------*/
+#ifndef __REG_STEER_APPSIDE_WSRF_H__
+#define __REG_STEER_APPSIDE_WSRF_H__
+
+/** @file ReG_Steer_Appside_WSRF.h
+    @brief Header file for WSRF over SOAP communications for the 
+    steered application
+  */
+
+/*-------------------------------------------------------------------*/
+
+/** Set up the connection to the SWS and publish the supported
+    commands to it 
+    @internal */
+int Initialize_steering_connection_wsrf(int  NumSupportedCmds,
+					int *SupportedCmds);
+/** Acknowledge a detach command from client
+    NOT USED
+    @internal */
+int Detach_from_steerer_wsrf();
+/** Check whether a steering client has connected to the SWS
+    @internal */
+int Steerer_connected_wsrf ();
+/** Clean-up the connection to the SWS
+    @internal */
+int Finalize_steering_connection_wsrf ();
+/** Send the supplied status message to the SWS 
+    @internal */
+int Send_status_msg_wsrf (char *msg);
+/** Get the next control msg that has been sent to the app
+    from a steering client 
+    @internal */
+struct msg_struct *Get_control_msg_wsrf ();
+/** Save a log (parameter or checkpoint) to the SWS 
+    @internal */
+int Save_log_wsrf (char *log_data);
+/*
+int 	Get_data_source_address_wsrf (int index, char *hostname, 
+				      unsigned short int *port);
+int 	Record_checkpoint_set_wsrf (char *chk_data, char *node_data);
+*/
+
+#endif
---
+++ include/ReG_Steer_Steerside_WSRF.h
@@ -0,0 +1,89 @@
+/*----------------------------------------------------------------------------
+  This header file contains routines and data structures for
+  steerside WSRF, SOAP-based communication.
+
+  (C) Copyright 2005, University of Manchester, United Kingdom,
+  all rights reserved.
+
+  This software was developed by the RealityGrid project
+  (http://www.realitygrid.org), funded by the EPSRC under grants
+  GR/R67699/01 and GR/R67699/02.
+
+  LICENCE TERMS
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions
+  are met:
+  1. Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+
+  THIS MATERIAL IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. THE ENTIRE RISK AS TO THE QUALITY
+  AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE PROGRAM PROVE
+  DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR
+  CORRECTION.
+
+  Authors........: Andrew Porter
+
+---------------------------------------------------------------------------*/
+#ifndef __REG_STEER_STEERSIDE_WSRF_H__
+#define __REG_STEER_STEERSIDE_WSRF_H__
+
+/** @file ReG_Steer_Steerside_WSRF.h
+    @brief Header file for WSRF over SOAP communications for the steering client.
+  */
+
+/*-------------------------------------------------------------------*/
+
+/** (For debugging.) Handler passed to gsoap to be called when get 
+    unexpected tags 
+    @internal */
+int soapMismatchHandler(struct soap *soap, 
+			const char  *tag);
+/** Attach to a simulation 
+    @internal */
+int Sim_attach_wsrf (Sim_entry_type *sim, 
+                     char           *SimID);
+/** Send a control msg to a simulation 
+    @internal */
+int Send_control_msg_wsrf (Sim_entry_type *sim,
+			   char           *buf);
+/** Send a detach msg to a simulation */
+int Send_detach_msg_wsrf (Sim_entry_type *sim);
+/** Get the next status message from the simulation 
+    @internal */
+struct msg_struct *Get_status_msg_wsrf(Sim_entry_type *sim);
+/** Retrieve the next stored message (if any) from the simulation.
+    @return NULL if no stored msg otherwise ptr to next message.
+    @internal */
+struct msg_struct *Get_next_stored_msg(Sim_entry_type *sim);
+/** Get the value of the specified resource property 
+    @internal */
+char *Get_resource_property (SGS_info_type *sgs_info, 
+			     char           *name);
+/** Get the whole resource property document 
+    @internal */
+char *Get_resource_property_doc(SGS_info_type *sgs_info);
+/** Clean up a WSRF-based steering connection 
+    @internal */
+int Finalize_connection_wsrf (Sim_entry_type *sim);
+/** Retrieve the full log of the parameter with the specified
+    handle. Returns REG_SUCCESS or REG_FAILURE.  If successful
+    then log data is stored in internal buffer.
+    @internal */
+int Get_param_log_wsrf(Sim_entry_type *sim,
+		       int             handle);
+/** Instruct the simulation to restart from the specified
+    node in a checkpoint tree. 
+    @internal
+    The Restart method of the SWS, contacts the specified checkpoint
+    node and obtains the information needed to identify the checkpoint
+    files to the simulation. */
+int Send_restart_msg_wsrf(Sim_entry_type *sim, char *chkGSH);
+
+#endif
