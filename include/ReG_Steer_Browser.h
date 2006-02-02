9651a9be7bdd220f3b6f6f172da642e16c444d1b
Andrew Porter <andrew.porter@manchester.ac.uk>
2006-02-02 10:17:38 +0000

    added extern PREFIX stuff so can call from C++

--- include/ReG_Steer_Browser.h
+++ include/ReG_Steer_Browser.h
@@ -40,67 +40,32 @@
 #ifdef __cplusplus
   #define PREFIX "C"
 #else
-  #define PREFIX 
+  #define PREFIX
 #endif
 
 /**
-  Holds details on an entry in the registry 
+  Holds details on a entry in the registry 
 */
 struct registry_entry {
 
-  /** The type of service (SWS, SGS, SGSFactory etc.) */
   char service_type[REG_MAX_STRING_LENGTH];
-  /** The endpoint of the service */
   char gsh[REG_MAX_STRING_LENGTH];
-  /** The endpoint of the service modelling the registry entry */
-  char entry_gsh[REG_MAX_STRING_LENGTH];
-  /** Name of the application */
   char application[REG_MAX_STRING_LENGTH];
-  /** Date and time at which application started */
   char start_date_time[REG_MAX_STRING_LENGTH];
-  /** User who lauched the application */
   char user[REG_MAX_STRING_LENGTH];
-  /** The group to which the user belongs */
   char group[REG_MAX_STRING_LENGTH];
-  /** Description of purpose of job */
   char job_description[REG_MAX_STRING_LENGTH];
 
 };
 
 /*-------------------------------------------------------------------*/
 
-/**
-   Really just wraps Get_registry_entries.  Uses the registry 
-   address in REG_REGISTRY_ADDRESS if set, otherwise uses
-   default (#define'd at the top of ReG_Steer_Browser.c).
-   Returns list of steerable applications. The Grid
-   Service Handle returned in simGSH must be supplied as the SimID
-   to Sim_attach. */
-extern PREFIX int Get_sim_list(int   *nSims,
-			       char **simName,
-			       char **simGSH);
-
 /** Queries specified registry
-    The pointer held in *entries must be free'd once the data
-    has been used.
-    @param registryGSH Address of the ServiceGroup/Registry to query
-    @param num_entries Number of entries in the registry
-    @param entries Array of structs holding details for each entry
+    @return The number of entries in the registry
+    @return Details for each entry
  */
-extern PREFIX int Get_registry_entries(const char *registryGSH, 
-				       int *num_entries,  
-				       struct registry_entry **entries);
+extern PREFIX int Get_registry_entries(char *registryGSH, 
+			               int *num_entries,  
+			               struct registry_entry **entries);
 
-/** Queries specified registry and filters results using the
-    supplied string.
-    An entry is matched if the supplied string appears in one or more
-    of its service_type, application, user, group, start_date_time or
-    job_description fields.
-    @see Get_registry_entries
-    @param pattern String holding pattern to be used in filtering
- */
-extern PREFIX int Get_registry_entries_filtered(const char *registryGSH, 
-						int *num_entries,  
-						struct registry_entry **entries,
-						char *pattern);
 #endif
