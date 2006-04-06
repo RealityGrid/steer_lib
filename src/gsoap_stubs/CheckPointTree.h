b02ac7c3436f2ba0e020359ed4d0e3ab82ef697a
Andrew Porter <andrew.porter@manchester.ac.uk>
2006-04-06 16:58:28 +0000

    For WSRF form of tree

---
+++ src/gsoap_stubs/CheckPointTree.h
@@ -0,0 +1,40 @@
+//gsoap cpt schema namespace: http://www.RealityGrid.org/CheckPointTree
+//gsoap apachesoap schema namespace: http://xml.apache.org/xml-soap
+//gsoap soapenc schema namespace: http://schemas.xmlsoap.org/soap/encoding/
+
+//gsoap cpt service namespace: http://www.RealityGrid.org/CheckPointTree
+
+//gsoap cpt service location: http://vermont.mvc.mcc.ac.uk:50000/Session/CheckPointTree
+//gsoap cpt service name: soapCheckPointTree
+
+struct cpt__findByHandleResponse {
+	xsd__string  _findByHandleReturn;
+};
+
+struct cpt__getActiveTreesResponse {
+	xsd__string  _getActiveTreesReturn;
+};
+
+struct cpt__createNewTreeResponse {
+	xsd__string  _createNewTreeReturn;
+};
+
+struct cpt__destroyResponse {
+       void *_;
+};
+
+//gsoap cpt service method-action: createNewTree ""
+cpt__createNewTree( xsd__string  in0, xsd__string  in1, 
+		    xsd__string  in2, xsd__string  in3, 
+		    struct cpt__createNewTreeResponse * out );
+//gsoap cpt service method-action: findByHandle ""
+cpt__findByHandle( xsd__string  in0, xsd__string  in1, 
+		   struct cpt__findByHandleResponse * out );
+//gsoap cpt service method-action: destroy ""
+cpt__destroy( void *_, 
+	      struct cpt__destroyResponse * out );
+//gsoap cpt service method-action: getActiveTrees ""
+cpt__getActiveTrees( void *_, 
+		     struct cpt__getActiveTreesResponse * out );
+
+
---
+++ src/gsoap_stubs/CheckPointTreeNode.h
@@ -0,0 +1,26 @@
+//gsoap cptn schema namespace: http://www.RealityGrid.org/CheckPointTreeNode
+//gsoap apachesoap schema namespace: http://xml.apache.org/xml-soap
+//gsoap soapenc schema namespace: http://schemas.xmlsoap.org/soap/encoding/
+//gsoap cptn service namespace: http://www.RealityGrid.org/CheckPointTreeNode
+//gsoap cptn service location: http://foo.bar/
+//gsoap cptn service name: soapCheckPointTreeNode
+
+struct cptn__addNodeResponse {
+  xsd__string  _addNodeReturn;
+};
+
+struct cptn__destroyResponse {
+  void *_;
+};
+
+//gsoap rgt service method-action: destroy ""
+cptn__destroy( void *_, 
+	       struct cptn__destroyResponse * out );
+
+//gsoap rgt service method-action: addNode ""
+cptn__addNode( xsd__string  in0, 
+	       xsd__string  in1, 
+	       xsd__string  in2, 
+	       xsd__string  in3, 
+	       xsd__string  in4, 
+	       struct cptn__addNodeResponse * out );
