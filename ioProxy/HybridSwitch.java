a9a2edb8aed21146b4f001982be5a491ab41e526
Andrew Porter <andrew.porter@manchester.ac.uk>
2006-05-12 16:33:56 +0000

    Modified version of the switch originally by Matt Harvey

---
+++ ioProxy/HybridSwitch.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class HybridSwitch {
+
+protected Hashtable threads_by_id;
+
+public HybridSwitch( int port ) throws InstantiationException {
+	try {
+	threads_by_id = new Hashtable();
+
+	ServerSocket serv = new ServerSocket( port );
+
+	for(;;) {
+		Socket sock = serv.accept();
+		new HybridThread( sock, this );
+	}
+	} catch( Exception ex ) { 
+	    throw new InstantiationException( ex.toString() );
+	} 
+}
+
+protected void send( String from,
+		     String to,
+		     String id, 
+		     int length, 
+		     byte[] data ) {
+
+	HybridThread thr = (HybridThread) threads_by_id.get( from );
+
+	if( thr!=null ) {
+		System.out.println( "Sending from ["+from+"]" );
+		thr.send( from, id, data );
+	}
+	else {
+		System.out.println( "Sending from ["+from+"] failed: NO destination" ); 	
+	}
+
+}
+
+protected void register_thread( String srcID, HybridThread thr ) {
+	System.out.println( "Registering subscriber to  ["+srcID+"]" ); 
+	if( srcID!=null ) {
+		threads_by_id.put( srcID, thr );
+	}
+}
+
+protected void deregister_thread( String id ) {
+	System.out.println( "Deregistering ["+id+"]" ); 
+	if( id!=null ) {
+		threads_by_id.remove( id );
+	}
+}
+
+
+
+public static void main( String args[] ) {
+	try {
+		new HybridSwitch( Integer.parseInt(args[0]) );
+	} catch( Exception ex ) {
+		System.out.println( "Syntax: HybridSwitch [portnum]" ); 
+	} 
+
+} 
+
+}
---
+++ ioProxy/HybridThread.java
@@ -0,0 +1,172 @@
+import java.io.*;
+import java.net.*;
+
+public class HybridThread extends Thread {
+
+HybridSwitch sw;
+Socket s;
+PrintStream os;
+InputStream in;
+String this_id;
+String src_id;
+
+public HybridThread( Socket s, HybridSwitch sw ) {
+	try {
+
+	this.s  = s;
+	this.sw = sw;
+
+	os = new PrintStream( s.getOutputStream() );
+	in = s.getInputStream();
+
+	byte c = ' ';
+	int idx=0;
+	byte buffer[] = new byte[1024];
+	while( c!='\n' && idx < buffer.length ) {
+	    c = (byte) in.read();
+	    if( c!='\n' ) {
+		buffer[idx++] = c;
+	    }
+	}		
+
+	// The ID of this connection
+	this_id = new String( buffer );
+	System.out.println( "--["+this_id+"]" ); 
+	this_id = this_id.trim();
+
+        int max = idx;
+	idx=0;
+        while(idx < max){
+	    buffer[idx++]=(byte)0;
+	}
+
+	c = ' ';
+	idx=0;
+	while( c!='\n' && idx < buffer.length ) {
+	    c = (byte) in.read();
+	    if( c!='\n' ) {
+		buffer[idx++] = c;
+	    }
+	}		
+
+	// The ID of the data source it's subscribing to (or "NO_DATA"
+	// if we're output only)
+	src_id = new String( buffer );
+	System.out.println( "--["+src_id+"]" ); 
+	src_id = src_id.trim();
+
+	sw.register_thread( src_id, this );
+
+	start();
+
+	} catch( Exception ex ) { ex.printStackTrace( System.err ); } 
+}
+
+
+public void run() {
+	try {
+	byte buffer[] = new byte[1024];
+
+	for(;;) {
+
+		byte c = ' ';
+
+System.out.println( "Reading..." ); 
+		while( c!='#' ) {
+			c = (byte) in.read();
+			if( c==-1 ) { throw new Exception(); }
+System.out.println( "B " ); 
+		}
+		
+System.out.println( "Reading...B" ); 
+		int idx=0;
+		buffer = new byte[1024];
+		while( c!='\n' && idx < buffer.length ) {
+				c = (byte) in.read();
+			if( c==-1 ) { throw new Exception(); }
+				if( c!='\n' ) {
+					buffer[idx++] = c;
+				}
+		}		
+
+		String to     = new String( buffer );
+		System.out.println( "--["+to+"]" ); 
+
+		c = ' ';
+		idx=0;
+		buffer = new byte[1024];
+		while( c!='\n' && idx < buffer.length ) {
+				c = (byte) in.read();
+			if( c==-1 ) { throw new Exception(); }
+				if( c!='\n' ) {
+					buffer[idx++] = c;
+				}
+		}		
+
+
+		String id     = new String( buffer );
+		System.out.println( "--["+id+"]" ); 
+
+		c = ' ';
+		idx=0;
+		buffer = new byte[1024];
+		while( c!='\n' && idx < buffer.length ) {
+				c = (byte) in.read();
+			if( c==-1 ) { throw new Exception(); }
+				if( c!='\n' ) {
+					buffer[idx++] = c;
+				}
+		}		
+
+
+		String length_s = new String( buffer );
+		length_s = length_s.trim();
+		System.out.println( "--["+length_s+"]" ); 
+
+		int length = Integer.parseInt( length_s );
+		int cur = 0;
+		int j;
+
+		byte[] b = new byte[ length ];
+
+		while( length-cur > 0 ) {
+			j = in.read( b, cur, length-cur );
+
+			if( j==-1 ) {
+				throw new Exception();
+			}
+			cur+=j;
+			System.out.println( "Read "+cur +" of "+ length ); 
+		}
+
+		to = to.trim();
+		id = id.trim();
+
+		sw.send( this_id, to, id, length, b );
+	}
+	} catch( Exception ex ) {
+		ex.printStackTrace(System.err);
+		close();
+	} 
+}
+
+public synchronized void send( String from, String id, byte[] data ) {
+	try {
+	    //os.print("\n#"+from+"\n"+id+"\n"+data.length+"\n" );
+		os.write( data );	
+
+	
+	} catch( Exception ex ) { 
+		ex.printStackTrace( System.err ); 
+		close();
+	} 
+}
+
+private void close() {
+		try {
+			s.close();			
+			in.close();
+		} catch( Exception ex ) { }
+		sw.deregister_thread( src_id );
+}
+}
