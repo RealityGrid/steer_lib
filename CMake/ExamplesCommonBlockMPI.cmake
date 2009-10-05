7fb81d82da2c5f873273b4fc35b13544cc1e0ba0
Robert Haines <rhaines@manchester.ac.uk>
2009-10-05 17:15:40 +0100

    Update the licence in each file. Also add a LICENCE file at the top-level.

--- CMake/ExamplesCommonBlockMPI.cmake
+++ CMake/ExamplesCommonBlockMPI.cmake
@@ -1,35 +1,48 @@
 #
-#  Common CMake build rules for the RealityGrid example MPI applications.
+#  The RealityGrid Steering Library
 #
-#  (C) Copyright 2008, University of Manchester, United Kingdom,
-#  all rights reserved.
+#  Copyright (c) 2002-2009, University of Manchester, United Kingdom.
+#  All rights reserved.
 #
-#  This software was developed by the RealityGrid project
-#  (http://www.realitygrid.org), funded by the EPSRC under grants
-#  GR/R67699/01 and GR/R67699/02.
+#  This software is produced by Research Computing Services, University
+#  of Manchester as part of the RealityGrid project and associated
+#  follow on projects, funded by the EPSRC under grants GR/R67699/01,
+#  GR/R67699/02, GR/T27488/01, EP/C536452/1, EP/D500028/1,
+#  EP/F00561X/1.
 #
 #  LICENCE TERMS
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions
 #  are met:
-#  1. Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-#  2. Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in the
-#     documentation and/or other materials provided with the distribution.
 #
-#  THIS MATERIAL IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#    * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+#    * Neither the name of The University of Manchester nor the names
+#      of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written
+#      permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-#  A PARTICULAR PURPOSE ARE DISCLAIMED. THE ENTIRE RISK AS TO THE QUALITY
-#  AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE PROGRAM PROVE
-#  DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR
-#  CORRECTION.
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
 #
-#  Author.........: Robert Haines
-#
-#------------------------------------------------------------------------
+#  Author: Robert Haines
 
 include_directories(${PROJECT_SOURCE_DIR}/include ${MPI_INCLUDE_PATH})
 string(REGEX MATCH "^[^\\.]+" EX_APP_NAME ${EX_SRC_NAME})
